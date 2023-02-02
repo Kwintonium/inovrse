2d688d199851d4c24fd6fc6083c45fa801f6fa72
Quinn <qmeyer1995@gmail.com>
2023-02-01 20:54:25 -0500

    added password feature for development

---
+++ alert.js
@@ -0,0 +1,10 @@
+var password = "viwkofw242242@#$@";
+
+function passcheck() {
+    if(document.getElementById('pass1').value != password) {
+    return false;
+    }
+
+    if(document.getElementById('pass1').value == password) {
+    }
+}
